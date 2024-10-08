c925df40c20e136413098cfe3ae41ec2ecb058c4
Cullen Braeden <bc832@zoo.cs.yale.edu>
2024-10-08 18:14:34 -0400

    adjusted optimizer

--- starter-code/optimizer.c
+++ starter-code/optimizer.c
@@ -1,21 +1,11 @@
 #include "optimizer.h"
 
-void Optimizer(NodeList *funcdecls) {
-    // Apply optimizations until no more changes occur
-    bool changed;
-    do {
-        changed = false;
-        // Apply Constant Folding optimization
-        if (ConstantFolding(funcdecls)) {
-            changed = true;
-        }
-        // Apply Constant Propagation optimization
-        if (ConstProp(funcdecls)) {
-            changed = true;
-        }
-        // Apply Dead Code Elimination optimization
-        if (DeadAssign(funcdecls)) {
-            changed = true;
-        }
-    } while (changed);
+void Optimizer(NodeList *funcdecls) 
+{
+    bool changed = true;
+
+    while (changed)
+    {
+        changed = ConstantFolding(funcdecls) || ConstProp(funcdecls) || DeadAssign(funcdecls);
+    }
 }
